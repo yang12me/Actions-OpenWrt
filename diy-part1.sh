0ce1d447fa274e90cdbe0c3161749b1bb7ebaf38
yang12me <yang12me@hotmail.com>
2021-04-25 20:59:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,6 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+
+# Add OpenClash
+sed -i '$a src-git lienol https://github.com/vernesong/OpenClash' feeds.conf.default
